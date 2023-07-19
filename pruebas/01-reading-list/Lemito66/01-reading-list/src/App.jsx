f0b8661d13d4373c2bc8c9838d730506589ea9e4
Emill <lemito66@gmail.com>
2023-07-19 16:57:14 -0500

    renderizando componentes de lista de libros

--- pruebas/01-reading-list/Lemito66/01-reading-list/src/App.jsx
+++ pruebas/01-reading-list/Lemito66/01-reading-list/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
-import './App.css'
+// import './App.css'
 import { ListOfBooks } from './components/listOfBooks'
+import { BasicExample } from './components/CardInfo'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
       <ListOfBooks />
+      {/* <BasicExample /> */}
     </>
   )
 }
