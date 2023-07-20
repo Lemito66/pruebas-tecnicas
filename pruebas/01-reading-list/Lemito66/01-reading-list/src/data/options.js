33cfda781e91630031b385dd28158dc40a95ec99
Emill <lemito66@gmail.com>
2023-07-20 12:27:37 -0500

    opciones de filtro

---
+++ pruebas/01-reading-list/Lemito66/01-reading-list/src/data/options.js
@@ -0,0 +1,23 @@
+export const OPTIONS = [
+    {
+        "value": "1",
+        "label": "Todas"
+    },
+    {
+        "value": "2",
+        "label": "Fantasía"
+    },
+    {
+        "value": "3",
+        "label": "Ciencia Ficción"
+    },
+    {
+        "value": "4",
+        "label": "Zombies"
+
+    },
+    {
+        "value": "5",
+        "label": "Terror"
+    }
+]
