215185ddffead96c027228822b58b3d84ec1bf16
Emill <lemito66@gmail.com>
2023-07-20 16:58:02 -0500

    cambio de tamaño

--- pruebas/01-reading-list/Lemito66/01-reading-list/src/components/CardPicture.jsx
+++ pruebas/01-reading-list/Lemito66/01-reading-list/src/components/CardPicture.jsx
@@ -2,7 +2,7 @@ import Card from 'react-bootstrap/Card';
 
 export function CardPicture({cover}) {
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: '12rem' }}>
       <Card.Img variant="top" src={cover} />
     </Card>
   );
