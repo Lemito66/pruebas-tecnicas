e71d0241a54fdf79846747e10822ea8035d2f73c
Emill <lemito66@gmail.com>
2023-07-20 11:33:45 -0500

    filtrado por genero

--- pruebas/01-reading-list/Lemito66/01-reading-list/src/components/ListOfBooks.jsx
+++ pruebas/01-reading-list/Lemito66/01-reading-list/src/components/ListOfBooks.jsx
@@ -3,9 +3,10 @@ import { books } from "../data/books";
 import { BasicExample } from "./CardInfo";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import { GENRES } from "../data/genres";
 
 export function ListOfBooks() {
-  
   const [book, setBook] = useState(books.library);
   const [howManyBooks, setHowManyBooks] = useState(book.length);
 
@@ -14,10 +15,35 @@ export function ListOfBooks() {
     setBook(newBook);
     setHowManyBooks(newBook.length);
   };
+
+  const filterToGenre = (genre) => {
+    if (genre === "1") {
+      setBook(books.library);
+      setHowManyBooks(books.library.length);
+      return;
+    }
+    const newBook = books.library.filter(
+      (book) => book.book.genre === GENRES[genre]
+    );
+    setBook(newBook);
+    setHowManyBooks(newBook.length);
+  };
   return (
     <>
       <h1>Lista de libros</h1>
       <h2>Hay {howManyBooks} libros en la lista</h2>
+      <div>
+        <Form.Select
+          aria-label="Default select example"
+          onChange={(e) => filterToGenre(e.target.value)}
+        >
+          <option value="1">Todas</option>
+          <option value="2">Fantasía</option>
+          <option value="3">Ciencia Ficción</option>
+          <option value="4">Zombies</option>
+          <option value="5">Terror</option>
+        </Form.Select>
+      </div>
       <button onClick={() => console.log(book)}>Da clic aqui</button>
       <Row className="g-4">
         {book.map(({ book }) => {
