9367aba35923468bb2abc1e33f31e017bd4c775b
Emill <lemito66@gmail.com>
2023-07-20 12:28:12 -0500

    refactorizando filtro de peliculas

--- pruebas/01-reading-list/Lemito66/01-reading-list/src/components/ListOfBooks.jsx
+++ pruebas/01-reading-list/Lemito66/01-reading-list/src/components/ListOfBooks.jsx
@@ -3,8 +3,9 @@ import { books } from "../data/books";
 import { BasicExample } from "./CardInfo";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
 import { GENRES } from "../data/genres";
+import { Filter } from "./Filter";
+import { OPTIONS } from "../data/options";
 
 export function ListOfBooks() {
   const [book, setBook] = useState(books.library);
@@ -33,16 +34,7 @@ export function ListOfBooks() {
       <h1>Lista de libros</h1>
       <h2>Hay {howManyBooks} libros en la lista</h2>
       <div>
-        <Form.Select
-          aria-label="Default select example"
-          onChange={(e) => filterToGenre(e.target.value)}
-        >
-          <option value="1">Todas</option>
-          <option value="2">Fantasía</option>
-          <option value="3">Ciencia Ficción</option>
-          <option value="4">Zombies</option>
-          <option value="5">Terror</option>
-        </Form.Select>
+        <Filter filterToGenre={filterToGenre} options={OPTIONS} />
       </div>
       <button onClick={() => console.log(book)}>Da clic aqui</button>
       <Row className="g-4">
