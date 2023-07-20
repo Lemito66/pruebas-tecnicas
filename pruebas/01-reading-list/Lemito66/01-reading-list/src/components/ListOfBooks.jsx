b8c2df44a5ab0a45bd2a86c0b8badb639ac7d6bd
Emill <lemito66@gmail.com>
2023-07-20 16:58:18 -0500

    nuevo contenedor de lista de lecturas

--- pruebas/01-reading-list/Lemito66/01-reading-list/src/components/ListOfBooks.jsx
+++ pruebas/01-reading-list/Lemito66/01-reading-list/src/components/ListOfBooks.jsx
@@ -6,15 +6,28 @@ import Col from "react-bootstrap/Col";
 import { GENRES } from "../data/genres";
 import { Filter } from "./Filter";
 import { OPTIONS } from "../data/options";
+import Container from "react-bootstrap/Container";
 
 export function ListOfBooks() {
   const [book, setBook] = useState(books.library);
   const [howManyBooks, setHowManyBooks] = useState(book.length);
+  const [listOfReading, setListOfReading] = useState([]);
 
   const handleDeleteBook = (ISBN) => {
     const newBook = book.filter((book) => book.book.ISBN !== ISBN);
     setBook(newBook);
     setHowManyBooks(newBook.length);
+  };
+
+  const handleAddBook = (ISBN) => {
+    const index = book.findIndex((book) => book.book.ISBN === ISBN);
+    const newBook = book[index];
+    const spliceElement = book.splice(index, 1);
+    setListOfReading([...listOfReading, newBook]);
+    setBook(book);
+    setHowManyBooks(book.length);
+
+    console.log(index);
   };
 
   const filterToGenre = (genre) => {
@@ -36,19 +49,39 @@ export function ListOfBooks() {
       <div>
         <Filter filterToGenre={filterToGenre} options={OPTIONS} />
       </div>
-      <button onClick={() => console.log(book)}>Da clic aqui</button>
-      <Row className="g-4">
-        {book.map(({ book }) => {
-          const { cover, ISBN, title } = book;
-          return (
-            <Col key={ISBN} xs={12} md={4}>
-              <div onClick={() => handleDeleteBook(ISBN)}>
-                <CardPicture cover={cover} />
-              </div>
-            </Col>
-          );
-        })}
+      <button onClick={() => console.log(book, listOfReading)}>Da clic aqui</button>
+      <Container>
+      <Row>
+        <Col md={10}>
+          <div>
+            <Row className="g-4">
+              {book.map(({ book }) => {
+                const { cover, ISBN, title } = book;
+                return (
+                  <Col key={ISBN} xs={12} md={3}>
+                    <div onClick={() => handleAddBook(ISBN)}>
+                      <CardPicture cover={cover} />
+                    </div>
+                  </Col>
+                );
+              })}
+            </Row>
+          </div>
+        </Col>
+        <Col md={2} style={{ border: '1px solid black' }}>
+        {listOfReading.map(({ book }) => {
+                const { cover, ISBN, title } = book;
+                return (
+                  <Col key={ISBN} xs={12} md={3}>
+                    <div onClick={() => handleAddBook(ISBN)}>
+                      <CardPicture cover={cover} />
+                    </div>
+                  </Col>
+                );
+              })}
+        </Col>
       </Row>
+    </Container>
     </>
   );
 }
