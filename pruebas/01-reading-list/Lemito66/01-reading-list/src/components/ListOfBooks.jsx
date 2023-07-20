f1c43b7775e0b591fdd40613c73df6bbafc45659
Emill <lemito66@gmail.com>
2023-07-20 10:51:26 -0500

    eliminación de libro de la pantalla

--- pruebas/01-reading-list/Lemito66/01-reading-list/src/components/ListOfBooks.jsx
+++ pruebas/01-reading-list/Lemito66/01-reading-list/src/components/ListOfBooks.jsx
@@ -5,15 +5,28 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export function ListOfBooks() {
+  
+  const [book, setBook] = useState(books.library);
+  //const [howManyBooks, setHowManyBooks] = useState(book.length);
+
+  const handleDeleteBook = (ISBN) => {
+    const newBook = book.filter((book) => book.book.ISBN !== ISBN);
+    setBook(newBook);
+    //setHowManyBooks(newBook.length);
+  };
   return (
     <>
       <h1>Lista de libros</h1>
+      <h2>Hay {howManyBooks} libros en la lista</h2>
+      <button onClick={() => console.log(book)}>Da clic aqui</button>
       <Row className="g-4">
-        {books.library.map(({ book }) => {
+        {book.map(({ book }) => {
           const { cover, ISBN, title } = book;
           return (
             <Col key={ISBN} xs={12} md={4}>
-              <BasicExample cover={cover} />
+              <div onClick={() => handleDeleteBook(ISBN)}>
+                <BasicExample cover={cover} />
+              </div>
             </Col>
           );
         })}
